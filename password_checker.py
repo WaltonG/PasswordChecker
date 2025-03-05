d71c2ca7a7eaac42c83544c3c7bc60bb12242d73
Walton Goga <52596583+WaltonG@users.noreply.github.com>
2025-03-05 11:51:11 +0300

    check pawned password

---
+++ password_checker.py
@@ -0,0 +1,14 @@
+import requests
+
+def request_api_data(query_char):
+  url = 'https://api.pwnedpasswords.com/range/' + query_char
+  res = requests.get(url)
+  if res.status_code != 200:
+    raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+  return res
+
+def pwned_api_check(password):
+    # Check password if it exists in API response
+    pass
+
+request_api_data('40BD0')
